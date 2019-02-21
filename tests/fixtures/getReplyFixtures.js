8827bd91b316f17616eea00fc14a59b97b97cf28
Andrew <arhorn@smcm.edu>
2019-02-21 23:45:23 +0700

    added boards to Reply fixtures

--- tests/fixtures/getReplyFixtures.js
+++ tests/fixtures/getReplyFixtures.js
@@ -5,32 +5,38 @@ const getReplyFixtures = () => [
   {
     text: 'reply fixture 1',
     delete_password,
-    created_on: moment('2019-02-01')
+    created_on: moment('2019-02-01'),
+    board: test
   },
   {
     text: 'reply fixture 2',
     delete_password,
-    created_on: moment('2019-01-01')
+    created_on: moment('2019-01-01'),
+    board: test
   },
   {
     text: 'reply fixture 3',
     delete_password,
-    created_on: moment('2019-02-02')
+    created_on: moment('2019-02-02'),
+    board: test
   },
   {
     text: 'reply fixture 4',
     delete_password,
-    created_on: moment('2019-01-02')
+    created_on: moment('2019-01-02'),
+    board: test
   },
   {
     text: 'reply fixture 5',
     delete_password,
-    created_on: moment('2019-02-03')
+    created_on: moment('2019-02-03'),
+    board: test
   },
   {
     text: 'reply fixture 6',
     delete_password,
-    created_on: moment('2019-01-10')
+    created_on: moment('2019-01-10'),
+    board: test
   }
 ];
 
