63df0d876e0df28ce04b68cd1e65c4cc8d5380bd
Andrew <arhorn@smcm.edu>
2019-02-21 18:11:29 +0700

    added GET thread tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -97,11 +97,104 @@ suite('Functional Tests', () => {
     });
 
     suite('GET', () => {
-      test('gets a list of all threads', done => {
+      test('gets a list of most recent 10 bumped threads and 3 bumped replies threads', done => {
         chai
           .request(server)
           .get('/api/threads/test')
-          .query();
+          .end((err, res) => {
+            const { threads, replies } = JSON.parse(res.body);
+            assert.equal(res.status, 200, 'res.status should be 200');
+            assert(threads.length === 10, 'should be 10 threads');
+            assert(replies.length === 3, 'should be 3 replies');
+            threads.forEach((curThread, index) => {
+              if (index !== 0) {
+                const prevThread = threads[index - 1];
+                assert(
+                  prevThread.bumped_on.valueOf() <
+                    curThread.bumped_on.valueOf(),
+                  'the previous bumped_on should be before the current one'
+                );
+              }
+              assert.notProperty(
+                curThread,
+                'reported',
+                'should not have reported property'
+              );
+              assert.notProperty(
+                curThread,
+                'delete_password',
+                'should not have delete_password property'
+              );
+              assert.property(curThread, 'text', 'should have text property');
+              assert.isString(curThread.text, 'text should be a string');
+              assert.property(
+                curThread,
+                'created_on',
+                'should have created_on property'
+              );
+              assert.isNumber(
+                curThread.created_on.valueOf(),
+                'created_on should be valid moment object'
+              );
+              assert.property(
+                curThread,
+                'bumped_on',
+                'should have bumped_on property'
+              );
+              assert.isNumber(
+                curThread.bumped_on.valueOf(),
+                'bumped_on should be valid moment object'
+              );
+              assert.property(
+                curThread,
+                'replies',
+                'should have replies property'
+              );
+              assert.isArray(curThread.replies, 'replies should be an array');
+              assert.property(curThread, 'board', 'should have board property');
+              assert.isString(curThread.board, 'board should be a string');
+            });
+            replies.forEach((curReply, index) => {
+              if (index !== 0) {
+                const prevReply = replies[index - 1];
+                assert(
+                  prevReply.bumped_on.valueOf() < curReply.bumped_on.valueOf(),
+                  'the previous bumped_on should be before the current one'
+                );
+              }
+              assert.notProperty(
+                curReply,
+                'reported',
+                'should not have reported property'
+              );
+              assert.notProperty(
+                curReply,
+                'delete_password',
+                'should not have delete_password property'
+              );
+              assert.property(curReply, 'text', 'should have text property');
+              assert.isString(curReply.text, 'text should be a string');
+              assert.property(
+                curReply,
+                'created_on',
+                'should have created_on property'
+              );
+              assert.isNumber(
+                curReply.created_on.valueOf(),
+                'created_on should be valid moment object'
+              );
+              assert.property(
+                curReply,
+                'thread_id',
+                'should have thread_id property'
+              );
+              assert.isString(
+                curReply.thread_id,
+                'thread_id should be a string'
+              );
+            });
+            done();
+          });
       });
     });
 
