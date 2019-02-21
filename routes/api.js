546e388a29056fd766e49185e9e08cdc1a5029e7
Andrew <arhorn@smcm.edu>
2019-02-21 23:47:22 +0700

    begun adding GET threads method

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,25 @@
-/*
- *
- *
- *       Complete the API routing below
- *
- *
- */
-
 'use strict';
-
 const { Thread } = require('../models/thread');
 const { Reply } = require('../models/reply');
 
 module.exports = app => {
-  app.route('/api/threads/:board').post((req, res) => {
-    let thread = req.body;
-    thread.board = req.params.board;
-    const doc = new Thread(thread);
-    doc.save();
-    res.send(doc);
-  });
+  app
+    .route('/api/threads/:board')
+    .post((req, res) => {
+      let thread = req.body;
+      thread.board = req.params.board;
+      const doc = new Thread(thread);
+      doc.save();
+      res.send(doc);
+    })
+
+    .get((req, res) => {
+      board = req.params.board;
+      const threadList = Thread.find({ board })
+        .sort({ bumped_on: -1 })
+        .limit(10);
+      const replyList = Reply.find({ board });
+    });
 
   app.route('/api/replies/:board');
 };
