7a92fb85a0be747f3803be80cc76893c598115d2
Andrew <arhorn@smcm.edu>
2019-02-21 23:28:04 +0700

    added POST new thread route

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,17 @@
 
 'use strict';
 
-const expect = require('chai').expect;
+const { Thread } = require('../models/thread');
+const { Reply } = require('../models/reply');
 
 module.exports = app => {
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board').post((req, res) => {
+    let thread = req.body;
+    thread.board = req.params.board;
+    const doc = new Thread(thread);
+    doc.save();
+    res.send(doc);
+  });
 
   app.route('/api/replies/:board');
 };
