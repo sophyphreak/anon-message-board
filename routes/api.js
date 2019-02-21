ce5388ae9c735824bcfcdd23c6bcf82035bb182f
Andrew <arhorn@smcm.edu>
2019-02-22 00:38:15 +0700

    added async/await and GET method. GET now functions basicly but problem with mlab server'

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,38 @@
 'use strict';
+require('../db/mongoose');
 const { Thread } = require('../models/thread');
 const { Reply } = require('../models/reply');
 
 module.exports = app => {
   app
     .route('/api/threads/:board')
-    .post((req, res) => {
+    .post(async (req, res) => {
       let thread = req.body;
       thread.board = req.params.board;
       const doc = new Thread(thread);
-      doc.save();
+      await doc.save(err => err && console.log(err));
       res.send(doc);
     })
 
-    .get((req, res) => {
-      board = req.params.board;
-      const threads = Thread.find({ board })
-        .sort({ bumped_on: -1 })
-        .limit(10);
-      const replies = Reply.find({ board })
-        .sort({ bumped_on: -1 })
-        .limit(3);
-      res.send({ threads, replies });
+    .get(async (req, res) => {
+      try {
+        const board = req.params.board;
+        const threads = await Thread.find(
+          { board },
+          err => err && console.log(err)
+        )
+          .sort({ bumped_on: -1 })
+          .limit(10);
+        const replies = await Reply.find(
+          { board },
+          err => err && console.log(err)
+        )
+          .sort({ bumped_on: -1 })
+          .limit(3);
+        res.send({ threads, replies });
+      } catch (e) {
+        console.log(e);
+      }
     });
 
   app.route('/api/replies/:board');
