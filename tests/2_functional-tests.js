c9dc51c8c1de95c4a50ce52a731dda5d00851169
Andrew <arhorn@smcm.edu>
2019-02-22 13:37:52 +0700

    removed useless comment

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
- *
- *
- *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
- *       -----[Keep the tests in the same order!]-----
- *       (if additional are added, keep them at the very end!)
- */
-
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const moment = require('moment');
